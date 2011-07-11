a7440e78be49ab6eba3fa0186b61927e4ced351e
darkredz <darkredz@85f6c152-20fe-9daf-8fbc-0cd1749a0b22>
2011-07-11 17:30:28 +0000

    update example testsuite.php
    git-svn-id: http://arr-bdd.googlecode.com/svn/trunk@6 85f6c152-20fe-9daf-8fbc-0cd1749a0b22

--- testsuite.php
+++ testsuite.php
@@ -10,7 +10,11 @@ include_once './test_demo/spec2.php';
 include_once './test_demo/hello.php';
 
 
+$bdd = new ArrBDD();
+//$results = $bdd->run($specs, true);     //show subject
+$results = $bdd->run($specs);
+
 // Output the raw results
-//outputPre(runTest($specs));
-//outputJSON(runTest($specs, true));
-outputJSON(runTest($specs));
+//$bdd->outputPre($results);
+//$bdd->outputVarDump($results);
+$bdd->outputJSON($results);
